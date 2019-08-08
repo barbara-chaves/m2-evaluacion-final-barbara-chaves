99ff85ed243a7d50cbb5518fc0d020a76e483d6a
barbara-chaves <barbarabarcellos90@gmail.com>
2019-08-09 00:06:43 +0200

    add save data

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -13,100 +13,109 @@ let datasFromServer = [];
 const getDataFromServer = () => {
   fetch(`http://api.tvmaze.com/search/shows?q=${searchSerie}`)
     .then(response => response.json())
-    .then(data => {
-      printResultSeries(data);
-      datasFromServer = data;
-      return datasFromServer;
-    });
+    .then(data => saveData(data));
 };
 
-const printResultSeries = data => {
+const saveData = data => {
   for (const serie of data) {
+    if (serie.show.image) {
+      datasFromServer.push({
+        name: serie.show.name,
+        id: serie.show.id,
+        image: serie.show.image.medium
+      });
+    } else {
+      datasFromServer.push({
+        name: serie.show.name,
+        id: serie.show.id,
+        image: `https://via.placeholder.com/210x295/ffffff/666666/?text=${
+          serie.show.name
+        }`
+      });
+    }
+  }
+  localStorage.setItem("result", JSON.stringify(datasFromServer));
+};
+
+let dataList = [];
+const printResultSeries = () => {
+  dataList = JSON.parse(localStorage.getItem("result"));
+  for (const serie of dataList) {
     createResultElements(serie);
   }
 };
 
-const resultList = document.querySelector('.result-list');
+const resultUL = document.querySelector(".result-list");
+
 const createResultElements = serie => {
-  const newResult = document.createElement('li');
-  newResult.classList.add('result-list-item', 'js-normal');
-  newResult.dataset.index = serie.show.id;
+  const newResult = document.createElement("li");
+  newResult.classList.add("result-list-item", "js-normal");
+  newResult.dataset.index = serie.id;
   const newResultTitle = `<h3 class="result-list-item-title"> ${
-    serie.show.name
+    serie.name
   }</h3>`;
-  let newResultImg = '';
-  if (serie.show.image) {
-    newResultImg = `<img class="result-list-item-img" src="${
-      serie.show.image.medium
-    }">`;
-  } else {
-    newResultImg = `<img class="result-list-img" src="https://via.placeholder.com/210x295/ffffff/666666/?text=${
-      serie.show.name
-    }">`;
-  }
+  let newResultImg = "";
+  newResultImg = `<img class="result-list-item-img" src="${serie.image}">`;
   newResult.innerHTML = newResultImg + newResultTitle;
-  resultList.appendChild(newResult);
+  resultUL.appendChild(newResult);
 };
 
 const deletResultList = () => {
-  resultList.innerHTML = '';
+  datasFromServer = [];
 };
 
-const searchBtn = document.querySelector('#btn-input');
-
-const addEventInResultItems = () => {
-  const resultListItems = document.querySelectorAll('.result-list-item');
-  for (const item of resultListItems){
-    item.addEventListener('click', handleListClick);
-  }
-};
+const searchBtn = document.querySelector("#btn-input");
 
 const handleBtnClick = () => {
   deletResultList();
   getUserInput();
   getDataFromServer();
-  // addEventInResultItems();
+  printResultSeries();
+  // saveInnerHTMLInLocal();
 };
 
-searchBtn.addEventListener('click', handleBtnClick);
+searchBtn.addEventListener("click", handleBtnClick);
 
 // ---FAVORITES
+const favoritList = [];
 
-const favoritList = [{serieName: ' ', serieElement: ' '}];
-const saveSerieInlocal = (event) => {
-  for (let favIndex = 0; favIndex < favoritList.length; favIndex++){
-    favoritList.push({
-      serieName: event.currentTarget.innerText,
-      serieElement: event.currentTarget.innerHTML
-    });
+const printSerieInFavoritesList = () => {
+  for (const serie of favoritList) {
+    const newFavorite = document.createElement("li");
+    newFavorite.innerHTML = serie.element;
   }
-  localStorage.setItem('favorite', JSON.stringify(favoritList));
+};
+
+const saveSerieInlocal = event => {
+  for (let i = 0; i < resultListItems.length; i++) {
+    if (resultListItems[i] === event.currentTarget) {
+      console.log(i);
+      dataList[i].element = resultListItems[i].innerHTML;
+      favoritList.push(dataList[i]);
+      localStorage.setItem("favorite", JSON.stringify(favoritList));
+    }
+  }
   return favoritList;
 };
 
-
-
 // const getPrintedItems = () => document.querySelectorAll('.result-list-item');
-const printedFavoriteList = document.querySelector('.favorites-list');
 
-const printSerieInFavoritesList = () => {
-  for (const serieItem of favoritList){
-    let serie = document.createElement('li');
-    serie.innerHTML = serieItem.serieElement;
-    printedFavoriteList.appendChild(serie);
-  }
+const changeSerieBG = event => {
+  event.currentTarget.classList.replace("js-normal", "js-favorite");
 };
 
-const changeSerieBG = (event) => {
-  event.currentTarget.classList.replace('js-normal', 'js-favorite');
-};
-
-const handleListClick = (event) => {
+const handleListClick = event => {
   saveSerieInlocal(event);
+  // saveInnerHTMLInLocal();
   printSerieInFavoritesList();
   changeSerieBG(event);
 };
-
-
-const main = document.querySelector('.main');
-main.addEventListener('click', addEventInResultItems);
+let resultListItems = [];
+const addEventInResultItems = () => {
+  resultListItems = document.querySelectorAll(".result-list-item");
+  for (const item of resultListItems) {
+    item.addEventListener("click", handleListClick);
+  }
+};
+const main = document.querySelector(".main");
+main.addEventListener("click", addEventInResultItems);
